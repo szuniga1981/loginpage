c2e2d3eadf0e017fc9499f7262ad9d7e72a79aae
sebastian <szuniga1981@gmail.com>
2020-11-11 20:46:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/loginpage/ventana_accesso_cuenta.java
@@ -0,0 +1,14 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana_accesso_cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_accesso_cuenta);
+    }
+}
